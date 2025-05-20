9017103d406099f2ac2511777491f1eb5bc70d15
DaniloMP10 <zdanilo2014@hotmail.com>
2025-05-20 18:03:31 -0300

    Cria classe CompareScreen

---
+++ lib/screens/compare_screen.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CompareScreen extends ConsumerWidget{
+  const CompareScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    throw UnimplementedError();
+  }
+}
