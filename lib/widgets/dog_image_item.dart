99866cf71a2af0850511dd42e45a4045867a5e28
DaniloMP10 <zdanilo2014@hotmail.com>
2025-05-19 22:18:53 -0300

    Cria Widget da Lista de Cachorros

---
+++ lib/widgets/dog_image_item.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import '../models/dog_image.dart';
+
+class DogImageItem extends StatelessWidget {
+  final DogImage dog;
+  final double height;
+
+  const DogImageItem({
+    super.key,
+    required this.dog,
+    this.height = 200,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.network(
+          dog.url,
+          height: height,
+          fit: BoxFit.cover,
+          loadingBuilder: (context, child, loadingProgress) {
+            if (loadingProgress == null) return child;
+            return SizedBox(
+              height: height,
+              child: const Center(child: CircularProgressIndicator()),
+            );
+          },
+        ),
+        const SizedBox(height: 8),
+        Text(
+          'ID: ${dog.id}',
+          textAlign: TextAlign.center,
+          style: const TextStyle(fontSize: 12, color: Colors.black54),
+        ),
+      ],
+    );
+  }
+}
