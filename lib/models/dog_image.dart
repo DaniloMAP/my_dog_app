a3bcf5864003eb5481d3ccd1f0f851a85d991988
DaniloMP10 <zdanilo2014@hotmail.com>
2025-05-20 21:19:19 -0300

    Adiciona 'breedName' na classe DogImage

--- lib/models/dog_image.dart
+++ lib/models/dog_image.dart
@@ -1,10 +1,18 @@
 class DogImage {
   final String id;
   final String url;
+  final String breedName;
 
-  DogImage({required this.id, required this.url});
+  DogImage({required this.id, required this.url, required this.breedName});
 
   factory DogImage.fromJson(Map<String, dynamic> json) {
-    return DogImage(id: json['id'] as String, url: json['url'] as String);
+    final breeds = (json['breeds'] as List<dynamic>?) ?? [];
+    final name =
+        breeds.isNotEmpty ? (breeds[0]['name'] as String) : 'Sem informação de raça';
+    return DogImage(
+      id: json['id'] as String,
+      url: json['url'] as String,
+      breedName: name,
+    );
   }
 }
