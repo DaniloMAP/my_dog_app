d2bfbff9da7c50f522346cdf0905bd45cecafc30
DaniloMP10 <zdanilo2014@hotmail.com>
2025-05-20 21:10:55 -0300

    Aumenta para 15 a qt de imagens exibidas

--- lib/repositories/dog_repository.dart
+++ lib/repositories/dog_repository.dart
@@ -7,7 +7,7 @@ class DogRepository {
       'live_X8lkTVWj22BXfDaEudNnpod3jfglHJZQX3Vvn5JMp1DyiZ2Rv45jdyVWk2TstQWO';
   static const _baseUrl = 'https://api.thedogapi.com/v1/images';
 
-  Future<List<DogImage>> fetchDogImages({int limit = 10}) async {
+  Future<List<DogImage>> fetchDogImages({int limit = 15}) async {
     final uri = Uri.parse('$_baseUrl/search?limit=$limit');
     final response = await http.get(uri, headers: {'x-api-key': _apiKey});
     if (response.statusCode == 200) {
